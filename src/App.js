2420710e1fae382cb869e748c916656d8d622196
Sam Windsor <sam@samwindsor.com>
2025-11-18 18:44:54 -0500

    added v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Coach PI App
+          Coach PI App V2
         </p>
         <a
           className="App-link"
