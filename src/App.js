9092d7d259eaaf9f1e4569913e08ff8639677a5b
Sam Windsor <sam@samwindsor.com>
2025-11-18 17:05:10 -0500

    revise directory

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coach PI App
         </p>
         <a
           className="App-link"
